a25a1b8b1e133b0620eed226e5a56426dbbe692c
behrami <behram1993@gmail.com>
2018-05-01 14:08:44 -0400

    done

--- rocket_test.rb
+++ rocket_test.rb
@@ -9,28 +9,35 @@ class RocketTest < Minitest::Test
                           {name: 'Behram', colour: 'Red', flying: true})
   end
 
-  def test_lift_off_initialize
+  def test_lift_off_initialize_returns_true
     assert @Rocket.lift_off
   end
 
-  def test_lift_off_myRocket
+  def test_lift_off_myRocket_returns_false
     refute @myRocket.lift_off
   end
   # ----------------------------
 
-  def test_land_initialize
+  def test_land_initialize_returns_false
     refute @Rocket.land
   end
 
-  def test_land_myRocket
+  def test_land_myRocket_returns_true
     assert @myRocket.land
   end
   # ----------------------------
 
   def test_status_initialize
-    name = @Rocket.name
+    # name = @Rocket.name
+    @Rocket.name = 'Sean'
     actual_value = @Rocket.status
-    expected_value = "Rocket #{name} is ready for lift off!"
+    expected_value = "Rocket Sean is ready for lift off!"
+    assert_equal(expected_value, actual_value)
+  end
+
+  def test_status_myRocket
+    actual_value = @myRocket.status
+    expected_value = "Rocket Behram is flying through the sky!"
     assert_equal(expected_value, actual_value)
   end
 
